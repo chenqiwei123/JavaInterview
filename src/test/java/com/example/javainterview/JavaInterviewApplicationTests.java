fc0e9a8665625504dec6d8184f59ac6522a89452
runwsh <1049563035@qq.com>
2023-02-22 20:42:50 +0800

    一维数组存储原理理解

--- src/test/java/com/example/javainterview/JavaInterviewApplicationTests.java
+++ src/test/java/com/example/javainterview/JavaInterviewApplicationTests.java
@@ -10,4 +10,14 @@ class JavaInterviewApplicationTests {
     void contextLoads() {
     }
 
+    // 一维数组存储原理理解
+    public static void main(String[] args) {
+        int[] arr = new int[3];
+        arr[0]=1;
+        arr[1]=2;
+        arr[2]=3;
+        int[] arr1=arr;
+        arr1[2]=100;
+        System.out.printf(String.valueOf(arr[2]));
+    }
 }
