dd0ccafd5b663c5da909e1a98163ef0b2892575a
yaoguang <1216723763@qq.com>
2023-02-23 22:08:04 +0800

    day1

---
+++ YG/src/main/java/com/power/yg/YgApplication.java
@@ -0,0 +1,13 @@
+package com.power.yg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YgApplication.class, args);
+    }
+
+}
---
+++ YG/src/test/java/com/power/yg/YgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.power.yg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
